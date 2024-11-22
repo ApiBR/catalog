1485e69ffeddf8ad7697bfd1df7270829ef7d08c
Guilherme Branco Stracini <guilherme@guilhermebranco.com.br>
2024-11-22 02:59:02 +0000

    feat - add lcov to coverage format in configuration file

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
     coverage: {
-      reporter: ['text', 'html'],
+      reporter: ['text', 'html', 'lcov'],
     },
   },
 });
